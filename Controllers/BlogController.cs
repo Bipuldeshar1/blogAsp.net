53021a7b081c5c78cf79a8a95260aeee6cc524be
Bipuldeshar1 <desharbipul@gmail.com>
2024-04-22 21:52:34 +0545

    blog edit delete done

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 
 namespace blogg.Controllers
 {
@@ -22,6 +23,73 @@ namespace blogg.Controllers
             this.context = context;
             this.userManager = userManager;
             this.imageService = imageService;
+        }
+        [HttpGet]
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var product= await context.blogModels.FindAsync(id);
+            if(product == null)
+            {
+                return RedirectToAction("Read", "Blog");
+            }
+            var pro = new PostModel() { 
+            Title= product.Title,
+            Description= product.Description,
+          
+            };
+            ViewData["Id"] = product.Id;
+            ViewData["img"] = product.ImageUrl;
+            return View(pro);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id,PostModel postModel)
+        {
+            var product = await context.blogModels.FindAsync(id);
+            if (product == null)
+            {
+                return RedirectToAction("Read", "Blog");
+            }
+            if (ModelState.IsValid)
+            {
+                string Imageurl = "";
+                if (postModel.ImageFile != null && postModel.ImageFile.Length > 0)
+                {
+
+                    Imageurl = await imageService.uploadImageAsync(postModel.ImageFile);
+                }
+
+                product.Title = postModel.Title;
+                product.Description=postModel.Description;
+                product.ImageUrl = Imageurl;
+              
+                await context.SaveChangesAsync();
+                return RedirectToAction("Read", "Blog");
+
+            }
+            return View(product);
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product =await context.blogModels.FindAsync(id);
+            if(product == null)
+            {
+                return RedirectToAction("Read", "Blog");
+            }
+            context.blogModels.Remove(product);
+            context.SaveChanges();
+            return RedirectToAction("Read","Blog");
+        }
+
+        [HttpGet]
+        public IActionResult Detail(int id)
+        {
+            var blog=  context.blogModels.Include(e=>e.appUSer).SingleOrDefault(b=>b.Id==id);
+            return View(blog);
         }
 
         [HttpGet]
