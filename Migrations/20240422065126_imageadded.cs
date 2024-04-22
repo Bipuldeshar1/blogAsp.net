8df8d0e09ca15accedc8a93865ed5b307d271f44
Bipuldeshar1 <desharbipul@gmail.com>
2024-04-22 12:36:44 +0545

    img column added

---
+++ Migrations/20240422065126_imageadded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace blogg.Migrations
+{
+    /// <inheritdoc />
+    public partial class imageadded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "blogModels",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "blogModels");
+        }
+    }
+}
--- Models/BlogModel.cs
+++ Models/BlogModel.cs
@@ -9,6 +9,7 @@ namespace blogg.Models
         [Key]
         public int Id { get; set; }
         public string Title { get; set; }
+        public string ImageUrl { get; set; }
         public string Description { get; set; }
 
         [ForeignKey("appUSer")]
